632615156f71a6f3843b931912328e770f0567bb
largefullmoon <largefullmoon@gmail.com>
2024-08-11 00:47:39 +0700

    initialize flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from pymongo import MongoClient
+from flask import Flask, request, jsonify, send_file, Response, stream_with_context
+from flask_cors import CORS
+import hashlib
+from twilio.rest import Client
+import random
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})  # Adjust the origin to your React app's URL
+cors = CORS(app, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST", "OPTIONS"])
+app.app_context().push()
+
